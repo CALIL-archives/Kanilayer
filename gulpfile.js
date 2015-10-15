64d8133ff43617a31fe0826987b88e63f4437803
Ryuuji Yoshimoto <ryuuji.y@gmail.com>
2015-10-15 23:54:56 +0900

    gulpの修正

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,19 @@ var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
 var codo = require('gulp-codo');
 
-gulp.task('default', function () {
-    gulp.src('kanilayer.coffee')
-        .pipe(coffee({bare: true}).on('error', gutil.log))
-        .pipe(gulp.dest('./')
-    );
-    gulp.src('kanilayer.coffee')
+gulp.task('codo', function () {
+    return gulp.src('kanilayer.coffee')
         .pipe(codo({
             name: 'Kanilayer',
             title: 'Haika Layer Manager for OpenLayers3',
             readme: 'README.md'
         })
+    );
+});
+
+gulp.task('default', ['codo'], function () {
+    return gulp.src('kanilayer.coffee')
+        .pipe(coffee({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest('./')
     );
 });
